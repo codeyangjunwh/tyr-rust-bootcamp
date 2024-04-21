a39727033a292a45a8136f457fc50bf6dfe2ad3c
Konata9 <maochichen@163.com>
2024-04-21 22:04:47 +0800

    udpate

--- rcli/src/main.rs
+++ rcli/src/main.rs
@@ -1,3 +1,27 @@
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[command(name="rcli", version="0.1.0", author="Rust CLI", about, long_about=None)]
+struct Opts {
+    #[command(subcommand)]
+    cmd: Subcommand
+}
+
+#[derive(Debug, Parser)]
+enum Subcommand {
+    #[command(name="csv", about="Convert CSV to JSON")]
+    CSV(CSVOpts)
+}
+
+#[derive(Debug, Parser)]
+struct CSVOpts {
+    #[arg(short, long, about="Input file path")]
+    input:String,
+
+    #[arg(short, long, about="Output file path")]
+    
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, rust!");
 }
